f4ac7930b64594e0fb7a0db7ac38cb7f3c36329f
Ivan Smetanin <ivan.smetanin100@gmail.com>
2024-08-05 20:50:08 +0300

    базовая настройка main.tsx и создание перввых двух контроллеров через генерацию

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+	//после localhost будет идти api
+	app.setGlobalPrefix('api')
+	app.use(cookieParser())
+	app.enableCors({
+		origin: ['http://localhost:3001'],
+		credentials: true,
+		exposedHeaders: 'set-cookie'
+	})
 	await app.listen(3000)
 }
 bootstrap()
